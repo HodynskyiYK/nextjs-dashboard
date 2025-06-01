99028008d858bc792bf4d155cba6359f840f7fd8
y.hodynskyi <yurii.hodynskyi@rsli.com>
2025-06-01 16:40:30 +0300

    Initial commit

---
+++ src/app/(pages)/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Customers = () => {
+    return (
+        <h2>Customers page</h2>
+    );
+};
+
+export default Customers;
---
+++ src/app/(pages)/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Invoices = () => {
+    return (
+        <h2>Invoices page</h2>
+    );
+};
+
+export default Invoices;
