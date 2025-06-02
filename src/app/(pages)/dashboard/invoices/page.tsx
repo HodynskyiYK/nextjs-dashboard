15d5d7a4558f92844d8147ba4f5a12a587027d60
y.hodynskyi <yurii.hodynskyi@rsli.com>
2025-06-02 15:32:24 +0300

    Chapter 11: Adding Search and Pagination

--- src/app/(pages)/dashboard/invoices/page.tsx
+++ src/app/(pages)/dashboard/invoices/page.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import {NextPage} from 'next'
+import {lusitana} from '@/src/app/ui/fonts'
+import Search from '@/src/app/ui/search'
+import {CreateInvoice} from '@/src/app/ui/invoices/buttons'
+import {InvoicesTableSkeleton} from '@/src/app/ui/skeletons'
+import InvoicesTable from '@/src/app/ui/invoices/table'
+import {fetchInvoicesPages} from '@/src/app/lib/data'
+import Pagination from '@/src/app/ui/invoices/pagination'
 
-const Invoices = () => {
+interface IInvoices {
+    searchParams?: Promise<{
+        query?: string
+        page?: string
+    }>
+}
+
+const Invoices: NextPage<IInvoices> = async (props) => {
+    const searchParams = await props.searchParams
+    const query = searchParams?.query || ''
+    const currentPage = Number(searchParams?.page) || 1
+    const totalPages = await fetchInvoicesPages(query)
+
     return (
-        <h2>Invoices page</h2>
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+            </div>
+            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+                <Search placeholder="Search invoices..."/>
+                <CreateInvoice/>
+            </div>
+            <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton/>}>
+                <InvoicesTable query={query} currentPage={currentPage}/>
+            </Suspense>
+            <div className="mt-5 flex w-full justify-center">
+                <Pagination totalPages={totalPages} />
+            </div>
+        </div>
     );
 };
 
